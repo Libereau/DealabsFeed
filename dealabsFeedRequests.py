6a041d79129db8747db38753ee2694c54cccaaa9
Libero <41334665+Libereau@users.noreply.github.com>
2020-03-12 23:55:43 +0100

    Update dealabsFeedRequests.py

--- dealabsFeedRequests.py
+++ dealabsFeedRequests.py
@@ -6,18 +6,22 @@ import getpass
 from bs4 import BeautifulSoup
 import requests
 import time
+import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
-os.system('cls')
+os.system('clear')
+f = open("produitsDealabs.txt", 'w')
 print("\n[#] Dealabs RSS feed parser.")
-url = "<Your Url Here>"
+f.write("[#] Dealabs RSS feed parser.\n")
+
+url = "https://www.dealabs.com/rssx/keyword-alarm/nIXuN_M_dZMkHo96vKMt0q9rHkiuc7M5EI_JFfKNYVk."
 feed = feedparser.parse(url)
 
 if feed.bozo == 1:
     exit("\n    [!] Erreur réseau ou Url non spécifiée")
-
-print("\n ############################################### \n")
-
-print("Liste des produits : ")
 
 if feed.status != 200:
     exit("[!] RSS feed not reachable!")
@@ -26,16 +30,10 @@ else:
         entries = feed.entries
 
         for i in range(0, len(feed.entries)):
-            print("\n  [!] Nouveau produit : ")
-            print("      . "+feed.entries[i].title)
             summary = feed.entries[i].summary
             price = re.findall("<strong>(.*?)</strong",summary)
-            print("      . Prix - "+price[0].split(" ")[0])
             dateOfPublishing = feed.entries[i].published
-            print("      . Date de publication - "+dateOfPublishing)
             lienArticle = feed.entries[i].id
-            print("      . Lien - "+lienArticle)
-            lienArticle
             headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
             r = requests.get(lienArticle, headers=headers)
             soup = BeautifulSoup(r.text, 'html.parser')
@@ -43,4 +41,40 @@ else:
             temp = classe.text.replace("\t","")
             temp = temp.replace(" ","")
             temp = temp.replace("\n","")
-            print("      . Température - "+temp)
+
+            if temp[0] != "-" and temp != "Nouveau":
+                if int(temp.split("°")[0]) > 50:
+                    print("\n  [!] Nouveau produit : ")
+                    f.write("\n  [!] Nouveau produit : ")
+                    print("      . "+feed.entries[i].title)
+                    f.write("\n      . "+feed.entries[i].title)
+                    print("      . Prix - "+price[0].split(" ")[0])
+                    f.write("\n      . Prix - "+price[0].split(" ")[0])
+                    print("      . Date de publication - "+dateOfPublishing)
+                    f.write("\n      . Date de publication - "+dateOfPublishing)
+                    print("      . Lien - "+lienArticle)
+                    f.write("\n      . Lien - "+lienArticle)
+                    print("      . Température - "+temp)
+                    f.write("\n      . Température - "+temp)
+                    f.write("\n")
+
+
+print("\n")
+port = 465
+adresse = "your address"
+password = "your pass"
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+    server.login(adresse, password)
+
+    message = MIMEMultipart()
+    message["From"] = adresse
+    message["To"] = adresse
+    message["Subject"] = "Nouveaux deals !"
+    text = "Nouveaux deals, regarde la piece jointe !"
+
+    message.attach(MIMEText(open("/home/libero/Documents/programmes/DealabsFeed/produitsDealabs.txt").read()))
+
+    server.sendmail(adresse, adresse, text)
